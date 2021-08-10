40c5bdc6b7d14eb93bc03519e4cc8beb632409b1
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-08-10 13:01:41 +0000

    up

--- Scala/shared/src/test/scala/test.scala
+++ Scala/shared/src/test/scala/test.scala
@@ -9,6 +9,7 @@ import skanren.sexp._
 def nullo(x:SExp) = x=:=list()
 def caro(x:SExp,a:SExp) = exists(d=>x=:=cons(a,d))
 def cdro(x:SExp,d:SExp) = exists(a=>x=:=cons(a,d))
+def conso(a:SExp,d:SExp,x:SExp) = x=:=cons(a,d)
 
 def membero(x:SExp,l:SExp):Goal = conde(
   caro(l,x),
@@ -26,6 +27,8 @@ class ExampleSpec extends AnyFlatSpec with should.Matchers {
     printAll(q=>q=:=t||q=:=f) should be (Set(t,f))
     printAll(q=>caro(list(Sym("a"),Sym("b")),q)) should be (Set(Sym("a")))
     printAll(q=>conde(caro(list(Sym("q"),Sym("o"),q),Sym("a")))) should be (Set())
+    printAll(q=>exists(d0=>begin(cdro(list(Sym("q"),Sym("o"),q),d0),conde(caro(d0,Sym("a")))))) should be (Set())
+    //printAll(q=>conde(caro(list(Sym("q"),Sym("o"),q),Sym("a")),exists(d0=>begin(cdro(list(Sym("q"),Sym("o"),q),d0),conde(caro(d0,Sym("a"))))))) should be (Set())
     //printAll(q=>membero(Sym("a"),list(Sym("q"),Sym("o"),q))) should be (Set(Sym("a")))
   }
 }
