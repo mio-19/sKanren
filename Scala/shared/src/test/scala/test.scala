3db1a6b2c8ff6da05bb21c512a858a13ed8a3d3d
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-08-11 11:19:23 +0000

    up

--- Scala/shared/src/test/scala/test.scala
+++ Scala/shared/src/test/scala/test.scala
@@ -19,6 +19,17 @@ def membero(x:SExp,l:SExp):Goal = conde(
   ))
 )
 
+/*
+def lookupo(env:SExp,id:SExp,value:SExp):Goal = conde(
+  caro(env, cons(id, value)),
+  exists(d=>begin(!caro(env, cons(id, value)),cdro(env,d),lookupo(d,id,value)))
+)
+def evalo(env:SExp,code:SExp,result:SExp):Goal = conde(
+  lookupo(env,code,result),
+  ???
+)
+*/
+
 class ExampleSpec extends AnyFlatSpec with should.Matchers {
   it should "work" in {
     // some are ported from https://github.com/mbillingr/miniKANREN/blob/master/R7RS/test.scm
@@ -32,5 +43,6 @@ class ExampleSpec extends AnyFlatSpec with should.Matchers {
     printAll(q=>membero(Sym("a"),list(Sym("q"),Sym("o"),q))) should be (Set(Sym("a")))
     printAll(q=>membero(q,list(Sym("q"),Sym("o")))) should be (Set(Sym("q"),Sym("o")))
     printAll(q=>exists(d=>begin(q=/=d,membero(q,list(d,Sym("o")))))) should be (Set(Sym("o")))
+    //printAll(q=>begin(!membero(q,list(Sym("b"),Sym("o"),Sym("a"))),membero(q,list(Sym("q"),Sym("o"))))) should be (Set(Sym("q")))
   }
 }
