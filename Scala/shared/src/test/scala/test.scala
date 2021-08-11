5037b6b9cc6d1b81a359eaf65ba8515040d813d9
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-08-11 07:37:33 +0000

    up

--- Scala/shared/src/test/scala/test.scala
+++ Scala/shared/src/test/scala/test.scala
@@ -29,6 +29,6 @@ class ExampleSpec extends AnyFlatSpec with should.Matchers {
     printAll(q=>conde(caro(list(Sym("q"),Sym("o"),q),Sym("a")))) should be (Set())
     printAll(q=>exists(d0=>begin(cdro(list(Sym("q"),Sym("o"),q),d0),conde(caro(d0,Sym("a")))))) should be (Set())
     printAll(q=>conde(caro(list(Sym("q"),Sym("o"),q),Sym("a")),exists(d0=>begin(cdro(list(Sym("q"),Sym("o"),q),d0),conde(caro(d0,Sym("a"))))))) should be (Set())
-    //printAll(q=>membero(Sym("a"),list(Sym("q"),Sym("o"),q))) should be (Set(Sym("a")))
+    printAll(q=>membero(Sym("a"),list(Sym("q"),Sym("o"),q))) should be (Set(Sym("a")))
   }
 }
