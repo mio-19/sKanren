f9dde7e9bab1d3bda2fd58455ee90330ad5a1034
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-08-27 08:44:48 +0000

    up

--- Scala/shared/src/main/scala/skanren.scala
+++ Scala/shared/src/main/scala/skanren.scala
@@ -1,21 +1,52 @@
 package skanren
 
+import scala.collection.immutable.HashMap
+
 sealed trait Hole[T]
 
 final class UniqueHole[T](identifier: Symbol) extends Hole[T]
 
-// TODO
-final case class UnifyResult()
+final case class Substitution[T](hole: Hole[T], value: T)
+
+type SubstitutionAny = Substitution[Any]
+
+type Substitutions = HashMap[Hole[_], _]
+
+implicit class SubstitutionsOps(subst: Substitutions) {
+  def walk[T <: Unifiable](x: T): T = x.matchHole match {
+    case Some(hole) => subst.get(hole) match {
+      case Some(next) => this.walk(next.asInstanceOf[T])
+      case None => x
+    }
+    case None => x
+  }
+}
+
+object Substitutions {
+  def of[T](hole: Hole[T], value: T): Substitutions = HashMap((hole, value))
+
+  def unchecked[T, U](hole: Hole[T], value: U): Substitutions = of[Any](hole.asInstanceOf[Hole[Any]], value)
+}
 
 trait Unifiable {
-  type T
-  implicit protected val ev: T =:= this.type
+  type T <: Unifiable
+  //implicit protected val ev: this.type <:< T
+  implicit protected val ev: this.type =:= T
 
-  def unify(other: T): UnifyResult = ???
+  def unify(subst: Substitutions, other: T): Option[Substitutions] =
+    (subst.walk(this), subst.walk(other)) match {
+      case (self, other) =>
+        (self.matchHole, other.matchHole) match {
+          case (Some(self), None) => Some(Substitutions.unchecked(self, other))
+          case (None, Some(other)) => Some(Substitutions.unchecked(other, self))
+          case (Some(self), Some(other)) => Some(Substitutions.unchecked(self, other))
+          case (None, None) => this.unifyConcrete(subst, other)
+        }
+    }
 
-  protected def unifyConcrete(other: T): UnifyResult = throw new UnsupportedOperationException()
+  protected def unifyConcrete(subst: Substitutions, other: T): Option[Substitutions] = throw new UnsupportedOperationException()
 
-  def matchHole(x: T): Option[Hole[T]] = ???
+  def matchHole: Option[Hole[T]]
 }
 
 implicit class UnifiablePatternMatching[T <: Unifiable](x: T) {
@@ -27,6 +58,10 @@ sealed trait Holeable[A] extends Unifiable {
   override implicit protected val ev = implicitly
 }
 
-final case class HoleablePure[T](x: T)
+final case class HoleablePure[T](x: T) extends Holeable[T] {
+  override def matchHole = None
+}
 
-final case class HoleableHole[T](x: Hole[T])
+final case class HoleableHole[T](x: Hole[Holeable[T]]) extends Holeable[T] {
+  override def matchHole = Some(x)
+}
