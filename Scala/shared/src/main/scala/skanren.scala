5d887c6dea62948ed05b1dfd378b2b2e9d5dd2d4
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-09-01 10:03:23 +0000

    up

--- Scala/shared/src/main/scala/skanren.scala
+++ Scala/shared/src/main/scala/skanren.scala
@@ -89,3 +89,81 @@ implicit object UnitUnifier extends ConcreteAtomUnifier[Unit]
 implicit object StringUnifier extends ConcreteAtomUnifier[String]
 
 implicit object BooleanUnifier extends ConcreteAtomUnifier[Boolean]
+
+sealed trait Goal {
+  def &&(other: Goal) = GoalConde(ParVector(ParVector(this, other)))
+
+  def ||(other: Goal) = GoalConde(ParVector(ParVector(this), ParVector(other)))
+
+  def asConde(depth: Int): GoalConde = GoalConde(ParVector(ParVector(this)))
+}
+
+object Goal {
+  def apply(x: => Goal): Goal = GoalDelay(x)
+}
+
+sealed trait SimpleGoal extends Goal {
+  def tag: Int
+}
+
+object SimpleGoalTags {
+  val GoalUnify = 0
+  val GoalNegUnify = 1
+  val GoalType = 2
+  val GoalNegType = 3
+}
+
+final case class GoalUnify[T](x: T, y: T)(implicit unifier: Unifier[T]) extends SimpleGoal {
+  override def tag = SimpleGoalTags.GoalUnify
+
+}
+
+final case class GoalNegUnify[T](x: T, y: T)(implicit unifier: Unifier[T]) extends SimpleGoal {
+  override def tag = SimpleGoalTags.GoalNegUnify
+
+}
+
+final case class GoalType[T](hole: Hole[T], cls: ClassTag[_])(implicit unifier: Unifier[T]) extends SimpleGoal {
+  override def tag = SimpleGoalTags.GoalType
+
+}
+
+final case class GoalNegType[T](hole: Hole[T], cls: ClassTag[_])(implicit unifier: Unifier[T]) extends SimpleGoal {
+  override def tag = SimpleGoalTags.GoalNegType
+
+}
+
+final class GoalDelay(x: => Goal) extends Goal {
+  lazy val get: Goal = x
+}
+
+final case class GoalConde(clauses: ParVector[ParVector[Goal]]) extends Goal {
+  def &&(other: GoalConde): GoalConde = GoalConde(for {
+    a <- this.clauses
+    b <- other.clauses
+  } yield a ++ b)
+
+  def ||(other: GoalConde): GoalConde = GoalConde(this.clauses ++ other.clauses)
+
+
+  override def asConde(depth: Int): GoalConde = if (depth <= 0) super.asConde(depth) else {
+    val newDepth = depth - 1
+    if (clauses.isEmpty)
+      this
+    else
+      clauses.map(block => if (block.isEmpty) GoalConde.success else block.map(_.asConde(newDepth)).reduce(_ && _)).reduce(_ || _)
+  }
+}
+
+object GoalConde {
+  // xs can't be empty
+  def asConde(xs: ParVector[Goal], depth: Int): GoalConde = {
+    val newDepth = depth - 1
+    xs.map(_.asConde(depth)).reduce(_ && _)
+  }
+
+  val success: GoalConde = GoalConde(ParVector(ParVector()))
+  val failure: GoalConde = GoalConde(ParVector())
+
+  def ands(xs: ParVector[Goal]) = GoalConde(ParVector(xs))
+}
