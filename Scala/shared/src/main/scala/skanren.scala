1b40c2b8d041753334192f71b550f0ffae28d5a9
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-09-01 10:18:59 +0000

    up

--- Scala/shared/src/main/scala/skanren.scala
+++ Scala/shared/src/main/scala/skanren.scala
@@ -15,23 +15,47 @@ final class Hole[T](identifier: Symbol) {
   }
 }
 
-type SubstitutionStore = ParHashMap[Hole[_], _]
+type SubstitutionStore = ParHashMap[Hole[_], SubstitutionElement[_]]
+
+final case class SubstitutionElement[T](x: T, unifier: Unifier[T]) {
+}
+
+final case class Substitution[T](hole: Hole[T], x: T, unifier: Unifier[T]) {
+}
+
+object Substitution {
+  def unchecked[A](hole: Hole[_], x: A, unifier: Unifier[_]) =
+    Substitution[Any](hole.asInstanceOf[Hole[Any]], x, unifier.asInstanceOf[Unifier[Any]])
+}
 
 implicit class SubstitutionStoreOps(subst: SubstitutionStore) {
 
-  private def get[T](x: Hole[T]): Option[T] = subst.get(x).asInstanceOf[Option[T]]
+  private def get[T](x: Hole[T]): Option[T] = subst.get(x).map(x => x.x.asInstanceOf[T])
 
   def walk[T](x: T)(implicit unifier: Unifier[T]): T = (for {
     hole <- unifier.matchHole(x)
     next <- this.get(hole)
   } yield this.walk(next)).getOrElse(x)
+
+  def add[T](goal: GoalUnify[T]): Option[SubstitutionStore] = goal.unifier.unify(goal.x, goal.y)(subst).map(_._1)
+
+  def adds(xs: ParVector[GoalUnify[_]]): Option[SubstitutionStore] =
+    xs.foldLeft(Some(subst): Option[SubstitutionStore])((s, x) => s.flatMap(_.add(x)))
+
+  def diff(sub: SubstitutionStore): ParVector[Substitution[_]] = ParVector() ++ sub.filter((k, v) => subst.get(k) match {
+    case Some(raw) => {
+      assert(v == raw)
+      false
+    }
+    case None => true
+  }).map((k, v) => Substitution.unchecked(k, v.x, v.unifier))
 }
 
 object SubstitutionStore {
   inline def walk[T](x: T)(implicit unifier: Unifier[T]): Unifying[T] = subst => Some((subst, subst.walk(x)))
 
-  inline def addEntry[T](h: Hole[T], x: T): Unifying[Unit] =
-    subst => Some((if (subst.contains(h)) throw new IllegalArgumentException() else subst.updated(h, x)), ())
+  inline def addEntry[T](h: Hole[T], x: T)(implicit unifier: Unifier[T]): Unifying[Unit] =
+    subst => Some((if (subst.contains(h)) throw new IllegalArgumentException() else subst.updated(h, SubstitutionElement(x, unifier))), ())
 }
 
 type Unifying[T] = SubstitutionStore => Option[(SubstitutionStore, T)]
@@ -115,22 +139,30 @@ object SimpleGoalTags {
   val GoalNegType = 3
 }
 
-final case class GoalUnify[T](x: T, y: T)(implicit unifier: Unifier[T]) extends SimpleGoal {
+final case class GoalUnify[T](x: T, y: T)(implicit ev: Unifier[T]) extends SimpleGoal {
+  val unifier = ev
+
   override def tag = SimpleGoalTags.GoalUnify
 
 }
 
-final case class GoalNegUnify[T](x: T, y: T)(implicit unifier: Unifier[T]) extends SimpleGoal {
+final case class GoalNegUnify[T](x: T, y: T)(implicit ev: Unifier[T]) extends SimpleGoal {
+  val unifier = ev
+
   override def tag = SimpleGoalTags.GoalNegUnify
 
 }
 
-final case class GoalType[T](hole: Hole[T], cls: ClassTag[_])(implicit unifier: Unifier[T]) extends SimpleGoal {
+final case class GoalType[T](hole: Hole[T], cls: ClassTag[_])(implicit ev: Unifier[T]) extends SimpleGoal {
+  val unifier = ev
+
   override def tag = SimpleGoalTags.GoalType
 
 }
 
-final case class GoalNegType[T](hole: Hole[T], cls: ClassTag[_])(implicit unifier: Unifier[T]) extends SimpleGoal {
+final case class GoalNegType[T](hole: Hole[T], cls: ClassTag[_])(implicit ev: Unifier[T]) extends SimpleGoal {
+  val unifier = ev
+
   override def tag = SimpleGoalTags.GoalNegType
 
 }
@@ -168,4 +200,48 @@ object GoalConde {
   val failure: GoalConde = GoalConde(ParVector())
 
   def ands(xs: ParVector[Goal]) = GoalConde(ParVector(xs))
+}
+
+type NegSubstitution = ParVector /*or*/ [Substitution[_]]
+
+type NegSubstitutionStore = ParVector /*and*/ [NegSubstitution]
+
+implicit class NegSubstitutionStoreOps(negs: NegSubstitutionStore) {
+  /*
+  def addAndNormalize(subst: SubstitutionStore, xs: ParVector[GoalNegUnify[_]]): Option[NegSubstitutionStore] = {
+    val newNegs = (xs.map(NegSubstitutionStoreOps.run(subst, _)).flatten) ++ (negs.map(NegSubstitutionStoreOps.run(subst, _)).flatten)
+    if (newNegs.exists(_.isEmpty)) None else Some(newNegs)
+  }
+  */
+}
+
+object NegSubstitutionStoreOps {
+  private def transverse[T](xs: ParVector[Option[T]]): Option[ParVector[T]] = try {
+    Some(xs.map(_.get))
+  } catch {
+    case _: java.util.NoSuchElementException => None
+  }
+
+  /*
+  // None means success, Some(ParVector()) means failure.
+  // unchecked
+  private def run[T](subst: SubstitutionStore, x: Unifiable, y: Unifiable): Option[NegSubstitution] = Unifiable.unify(subst, x, y) match {
+    case None => None
+    case Some(newst) => Some(subst.diff(newst))
+  }
+
+  // None means success, Some(ParVector()) means failure.
+  private def run(subst: SubstitutionStore, goal: GoalNegUnify[_]): Option[NegSubstitution] = this.run(subst, goal.x, goal.y)
+
+  // None means success, Some(ParVector()) means failure.
+  private def run(subst: SubstitutionStore, element: Substitution[_]): Option[NegSubstitution] = element match {
+    case Substitution(hole, x) => subst.get(hole) match {
+      case Some(next) => this.run(subst, next, x)
+      case None => Some(ParVector(element))
+    }
+  }
+
+  // None means success, Some(ParVector()) means failure.
+  private def run(subst: SubstitutionStore, x: NegSubstitution): Option[NegSubstitution] = transverse(x.map(this.run(subst, _))).map(_.flatten)
+  */
 }
